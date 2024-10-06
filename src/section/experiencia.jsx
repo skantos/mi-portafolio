0fa1f8bca65db6cd4930966f8218496fbc50c377
fernet_en_mano <91861841+skantos@users.noreply.github.com>
2024-10-06 01:17:01 -0300

    TRABAJO

--- src/section/experiencia.jsx
+++ src/section/experiencia.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "../styles/experiencia.css";
 
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaPython, FaDatabase, FaGithub, FaDocker, FaCode } from "react-icons/fa";
+
 function experiencia() {
     return (
     <div>
         <div className="presentation_habilidades">
             <h1 className="habilidades_title">Experiencia</h1>
         </div>
-        
+
         <div className="bento-container">
             <div className="bento-item large">
                 <div className="bento-background"></div>
@@ -18,11 +20,16 @@ function experiencia() {
             </div>
 
             <div className="bento-item">
-                <div className="bento-background"></div>
-                <div className="bento-content">
-                    <h3>Elemento 1</h3>
-                    <p>Contenido breve.</p>
-                </div>
+                <a href="https://ppokedex-api-app.netlify.app/">
+                    <div className="bento-background"></div>
+                    <div className="bento-content">
+                        <h3>Pokedex</h3>
+                        <p>pokedex realizada con <br /> la api pokemon</p>
+                        <FaJs className="skill js" />
+                        <FaHtml5 className="skill html" />
+                        <FaCss3Alt className="skill css" />
+                    </div>
+                </a>
             </div>
 
             <div className="bento-item">
